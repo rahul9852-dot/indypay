ac3556f4d98c9dfba24ffb0f2b5afafb0dfefd18
Jahnavi-paybolt <jahnavi.g@paybolt.in>
2025-05-17 17:36:43 +0530

    Fix: Add Rejected status

--- src/utils/helperFunctions.utils.ts
+++ src/utils/helperFunctions.utils.ts
@@ -27,6 +27,8 @@ export const convertExternalPaymentStatusToInternal = (status: string) => {
       return PAYMENT_STATUS.SUCCESS;
     case "PROCESSED":
       return PAYMENT_STATUS.SUCCESS;
+    case "REJECTED":
+      return PAYMENT_STATUS.FAILED;
     case "200":
       return PAYMENT_STATUS.SUCCESS;
     case "400":
