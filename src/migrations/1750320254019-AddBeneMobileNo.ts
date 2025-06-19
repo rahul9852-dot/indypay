978436cd82b6624311a8d85e61251c2cf8b9b305
Jahnavi-paybolt <jahnavi.g@paybolt.in>
2025-06-19 13:44:10 +0530

    edit mig

--- src/migrations/1750320254019-AddBeneMobileNo.ts
+++ src/migrations/1750320254019-AddBeneMobileNo.ts
@@ -4,12 +4,12 @@ export class AddBeneMobileNo1750320254019 implements MigrationInterface {
   name = "AddBeneMobileNo1750320254019";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE TABLE "checkouts" ("id" character varying NOT NULL, "payerName" text, "payerEmail" text NOT NULL, "payerMobile" text, "payerAddress" text, "amount" numeric(10,2) NOT NULL, "clientTxnId" text NOT NULL, "status" character varying NOT NULL DEFAULT 'PENDING', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_50ff635b5d91851d87a54eeb9f7" UNIQUE ("clientTxnId"), CONSTRAINT "PK_5800730d89f4137fc18770e4d4d" PRIMARY KEY ("id"))`,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "IDX_e122b4131eba90a0becaa0e3b9" ON "checkouts" ("status") `,
-    );
+    // await queryRunner.query(
+    //   `CREATE TABLE "checkouts" ("id" character varying NOT NULL, "payerName" text, "payerEmail" text NOT NULL, "payerMobile" text, "payerAddress" text, "amount" numeric(10,2) NOT NULL, "clientTxnId" text NOT NULL, "status" character varying NOT NULL DEFAULT 'PENDING', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_50ff635b5d91851d87a54eeb9f7" UNIQUE ("clientTxnId"), CONSTRAINT "PK_5800730d89f4137fc18770e4d4d" PRIMARY KEY ("id"))`,
+    // );
+    // await queryRunner.query(
+    //   `CREATE INDEX "IDX_e122b4131eba90a0becaa0e3b9" ON "checkouts" ("status") `,
+    // );
     await queryRunner.query(
       `ALTER TABLE "payout_orders" ADD "beneficiaryMobile" character varying`,
     );
@@ -40,6 +40,6 @@ export class AddBeneMobileNo1750320254019 implements MigrationInterface {
     await queryRunner.query(
       `DROP INDEX "public"."IDX_e122b4131eba90a0becaa0e3b9"`,
     );
-    await queryRunner.query(`DROP TABLE "checkouts"`);
+    // await queryRunner.query(`DROP TABLE "checkouts"`);
   }
 }
