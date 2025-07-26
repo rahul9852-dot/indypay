7f1db185e0cdb6188e645573af62a1ffa7e2fea3
Jahnavi-paybolt <jahnavi.g@paybolt.in>
2025-07-26 23:50:45 +0530

    Mig file

---
+++ src/migrations/1753554011236-KybDocs.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class KybDocs1753554011236 implements MigrationInterface {
+  name = "KybDocs1753554011236";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "payin_orders" ALTER COLUMN "commissionInPercentage" SET DEFAULT '4.5'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "commissionInPercentagePayin" SET DEFAULT '4.5'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "commissionInPercentagePayout" SET DEFAULT '1.5'`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "commissionInPercentagePayout" SET DEFAULT 1.5`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "commissionInPercentagePayin" SET DEFAULT 4.5`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "payin_orders" ALTER COLUMN "commissionInPercentage" SET DEFAULT 4.5`,
+    );
+  }
+}
