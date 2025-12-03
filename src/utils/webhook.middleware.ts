ae9a8147dc1116d0bd92cef27abf4b763d25ae40
Jahnavi RF <jahnavi-rf>
2025-12-03 16:11:39 +0530

    webhook parser

--- src/utils/webhook.middleware.ts
+++ src/utils/webhook.middleware.ts
@@ -15,13 +15,39 @@ export const webhookBodyParser = (
     data += chunk;
   });
 
+  // req.on("end", () => {
+  //   // Add raw body to request
+  //   (req as any).rawBody = data;
+
+  //   try {
+  //     // req.body = JSON.parse(data);
+  //     req.body = Object.fromEntries(new URLSearchParams(data));
+  //     logger.info("Webhook body parsed successfully");
+  //   } catch (e) {
+  //     logger.error(
+  //       "Failed to parse webhook JSON:",
+  //       e instanceof Error ? e.message : "Unknown error",
+  //     );
+  //     req.body = {};
+  //   }
+  //   next();
+  // });
+
   req.on("end", () => {
-    // Add raw body to request
     (req as any).rawBody = data;
 
     try {
-      // req.body = JSON.parse(data);
-      req.body = Object.fromEntries(new URLSearchParams(data));
+      const body = data;
+
+      // Find JSON between multipart boundaries:
+      const match = body.match(/\r\n\r\n([\s\S]*?)\r\n-+/);
+
+      if (match && match[1]) {
+        req.body = JSON.parse(match[1]);
+      } else {
+        req.body = {};
+      }
+
       logger.info("Webhook body parsed successfully");
     } catch (e) {
       logger.error(
