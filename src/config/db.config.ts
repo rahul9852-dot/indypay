0d8286201d27cf32ac704d09d3b15ddf411a4d2d
Jahnavi RF <jahnavi-rf>
2026-02-28 19:32:58 +0530

    remove ssl for pgbouncer

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -21,20 +21,24 @@ export const dbConfig: TypeOrmModuleOptions = {
     // Target: 200+ req/sec with high success rate
     // Current issue: 45 req/sec with only 29 successful (64% success)
     // Need larger pool to prevent connection timeouts
-    max: 60, // Increased significantly to handle bursts and prevent timeouts
-    min: 10, // Higher min to reduce connection acquisition time
+    max: 20, // Increased significantly to handle bursts and prevent timeouts
+    // to 20 & 5 to test pg bouncer
+    min: 5, // Higher min to reduce connection acquisition time
     connectionTimeoutMillis: 15000, // More time when pool is busy
     idleTimeoutMillis: 30000,
     statement_timeout: 5000, // Balanced - not too aggressive
     lock_timeout: 1000, // Reasonable lock timeout
     idle_in_transaction_session_timeout: 2000, // Kill idle transactions
+    prepareThreshold: 0,
     // acquireTimeoutMillis: 20000, // Much more time to acquire connection during high load
     // reapIntervalMillis: 100, // Faster cleanup for better connection reuse
   },
   logging: !isProduction,
-  ...((isProduction || isStaging) && {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  }),
+  ...((isProduction || isStaging) &&
+    {
+      // commented ssl for pgbouncer
+      // ssl: {
+      //   rejectUnauthorized: false,
+      // },
+    }),
 };
